0d2f2ef39ada0accf0e26a72cf9a01eb9de5a7af
Ullas Zingade <65844617+UllasZ@users.noreply.github.com>
2021-08-31 19:46:11 +0530

    Update Question_2.py

--- Question_2.py
+++ Question_2.py
@@ -3,25 +3,19 @@ Convert the CSV to JSON
 A CSV file named q3_candidates.csv is attached. The file has the following headers:
 id - integer, name - string, email - string, phone - string, degree - string, major - string, institute - string
 title - string, company - string
-
 skills - list of lowercase strings
-
 1. Load the data from the CSV into a Python data structure and then print them.
-
 2. Make sure you load data into proper data structures and data types.
 Example:
 - id must be an integer.
 - “skills” is a list of lowercase strings. "Python,Django, Tensorflow" should
 be converted to [“python”, ”django”, “tensorflow”].
-
 3. Then store this data into a JSON file named “q3_candidates_processed.json”.
-
 4. NOTE:
 - Structure your code into appropriate functions.
 - Make sure you don’t hard-code the file paths of the CSV and JSON files. Use
 relative paths instead.
 - Don’t use any third party libraries like Pandas.
-
 5. HINT:
 - Store the data in a list of dictionaries where each item represents a row from the
 CSV."""
@@ -51,9 +45,9 @@ def formatting(data=None):
 
 # read csv data
 my_path = os.path.abspath(os.path.dirname(__file__))
-path = os.path.join(my_path, "q3_candidates.csv")
+csv_path = os.path.join(my_path, "q3_candidates.csv")
 
-with open(os.path.join(path), "r") as f:
+with open(os.path.join(csv_path), "r") as f:
     csv_reader = csv.reader(f)
     csv_reader = list(csv_reader)
     keys: object = csv_reader[0]
@@ -66,6 +60,8 @@ print("\nReading CSV data...... ")
 time.sleep(2)
 
 # dump to json
-with open(os.path.join(path), "w") as f:
+json_path = os.path.join(my_path, "q3_candidates.json")
+
+with open(os.path.join(json_path), "w") as f:
     json.dump(csv_data, f, indent=2)
     print("\nData dumped to the json file Successfully!")
